04d463565408c1441410a216b9479ebed6e426fb
rohit.wable <rohit.wable@ydv.io>
2019-09-13 19:19:33 +0530

    container - added burger-builder container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
 import Layout from './components/layout'
+import BurgerBuilder from './containers/burger-builder'
 class App extends Component {
   render() {
     return (
      <Layout>
-      children
+      <BurgerBuilder/>
      </Layout>
     )
   }
