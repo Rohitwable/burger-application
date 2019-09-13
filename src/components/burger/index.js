0fd10b8e057d751def1904a4e7f9b464f9bc2507
rohit.wable <rohit.wable@ydv.io>
2019-09-13 21:02:31 +0530

    - added logic to add the ingredients

--- src/components/burger/index.js
+++ src/components/burger/index.js
@@ -2,9 +2,22 @@ import React from 'react'
 import classes from './styles.css'
 import BurgerIngredient from './burger-ingredient'
 export default function burger(props) {
+    let transFormedIngredient = Object.keys(props.ingredients)
+    .map((ingKey => {
+        return [...Array(props.ingredients[ingKey])]
+        .map((_, i)=> <BurgerIngredient key={ingKey + i} type={ingKey}/>)
+    }))
+    .reduce((cur, nex)=> {
+        return cur.concat(nex)
+    }, [])
+    if(transFormedIngredient.length === 0){
+        transFormedIngredient = <p>Please start adding ingredients</p>
+    }
     return (
         <div className={classes.burger}>
-            <BurgerIngredient  type='bread-top'/>
+        <BurgerIngredient type='bread-top'/>
+            {transFormedIngredient}
+            <BurgerIngredient type='bread-bottom'/>
         </div>
     )
 }
--- src/containers/burger-builder/index.js
+++ src/containers/burger-builder/index.js
@@ -2,11 +2,19 @@ import React, { Component } from 'react'
 import Aux from '../../components/hoc/aux'
 import Burger from '../../components/burger'
  class BurgerBuilder extends Component {
+     state = {
+         ingredient : {
+             salad: 1,
+             cheese:2 ,
+             bacon: 2,
+             meat: 2
+         }
+     }
     render() {
         return (
             <Aux>
                 <div>
-                    <Burger/>
+                    <Burger ingredients ={this.state.ingredient}/>
                 </div>
                 <div>
                     Build controls
